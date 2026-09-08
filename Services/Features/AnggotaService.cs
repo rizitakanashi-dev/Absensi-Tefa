18a479f1f49121151a3a32b02178c48a7e41758f
rizitakanashi-dev <rizitakanashi@gmail.com>
2026-09-08 09:39:50 +0700

    feat: add update & delete (anggota, pm)

--- Services/Features/AnggotaService.cs
+++ Services/Features/AnggotaService.cs
@@ -23,7 +23,6 @@ namespace Absensi.Services
             try
             {
                 string hashedPassword = passwordService.HashPassword(data.password);
-
                 int? divisiId = data.id_divisi > 0 ? data.id_divisi : null;
 
                 string sql = @"INSERT INTO user(nama, password, id_role, id_divisi)
@@ -33,7 +32,7 @@ namespace Absensi.Services
                 {
                     nama = data.nama,
                     password = hashedPassword,
-                    id_role = 4,
+                    id_role = 4, // 4 = Anggota
                     id_divisi = divisiId
                 }, transaction);
 
@@ -58,6 +57,47 @@ namespace Absensi.Services
 
             var result = await conn.QueryAsync<UserDTO>(sql);
             return result.ToList();
+        }
+
+        public async Task<UserDTO?> GetById(int id)
+        {
+            using var conn = db.connect();
+            string sql = @"SELECT u.id, u.nama, r.nama AS role, d.nama AS divisi
+                           FROM user u
+                           JOIN role r ON r.id = u.id_role
+                           LEFT JOIN divisi d ON d.id = u.id_divisi
+                           WHERE u.id = @id AND u.id_role = 4;";
+
+            return await conn.QueryFirstOrDefaultAsync<UserDTO>(sql, new { id });
+        }
+
+        public async Task<bool> Update(int id, UserOTD data)
+        {
+            using var conn = db.connect();
+            int? divisiId = data.id_divisi > 0 ? data.id_divisi : null;
+
+            string sql = @"UPDATE user 
+                           SET nama = @nama, 
+                               id_divisi = @id_divisi 
+                           WHERE id = @id AND id_role = 4;";
+
+            var result = await conn.ExecuteAsync(sql, new
+            {
+                id,
+                nama = data.nama,
+                id_divisi = divisiId
+            });
+
+            return result > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using var conn = db.connect();
+            string sql = "DELETE FROM user WHERE id = @id AND id_role = 4;";
+            var result = await conn.ExecuteAsync(sql, new { id });
+
+            return result > 0;
         }
     }
 }
--- Services/Features/PMService.cs
+++ Services/Features/PMService.cs
@@ -6,53 +6,98 @@ namespace Absensi.Services
     public class PMService
     {
         private readonly Database db;
+        private readonly IPasswordService passwordService;
 
-        public PMService(Database _db) => db = _db;
+        public PMService(Database _db, IPasswordService _passwordService)
+        {
+            db = _db;
+            passwordService = _passwordService;
+        }
 
         public async Task<bool> Register(UserOTD data)
         {
-            using (var conn = db.connect())
+            using var conn = db.connect();
+            await conn.OpenAsync();
+
+            using var transaction = await conn.BeginTransactionAsync();
+            try
             {
-                await conn.OpenAsync();
+                string hashedPassword = passwordService.HashPassword(data.password);
+                int? divisiId = data.id_divisi > 0 ? data.id_divisi : null;
 
-                using (var transaction = await conn.BeginTransactionAsync())
+                string sql = @"INSERT INTO user(nama, password, id_role, id_divisi)
+                               VALUES(@nama, @password, @id_role, @id_divisi);";
+
+                int affectedRows = await conn.ExecuteAsync(sql, new
                 {
-                    try
-                    {
-                        string sql = @"INSERT INTO user(nama, password, id_role, id_divisi)
-                           VALUES(@nama, @password, @id_divisi, @id_role);";
-                        int affectedRows = await conn.ExecuteAsync(sql, new
-                        {
-                            nama = data.nama,
-                            password = data.password,
-                            id_role = 2,
-                            id_divisi = data.id_divisi
-                        }, transaction);
-                        await transaction.CommitAsync();
-                        return affectedRows > 0;
+                    nama = data.nama,
+                    password = hashedPassword,
+                    id_role = 2, // 2 = PM (Project Manager)
+                    id_divisi = divisiId
+                }, transaction);
 
-                    }
-                    catch
-                    {
-                        await transaction.RollbackAsync();
-                        throw;
-                    }
-                }
+                await transaction.CommitAsync();
+                return affectedRows > 0;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
             }
         }
 
         public async Task<List<UserDTO>> GetAll()
         {
-            using (var conn = db.connect())
+            using var conn = db.connect();
+            string sql = @"SELECT u.id, u.nama, r.nama AS role, d.nama AS divisi
+                           FROM user u
+                           JOIN role r ON r.id = u.id_role
+                           LEFT JOIN divisi d ON d.id = u.id_divisi
+                           WHERE u.id_role = 2;";
+
+            var result = await conn.QueryAsync<UserDTO>(sql);
+            return result.ToList();
+        }
+
+        public async Task<UserDTO?> GetById(int id)
+        {
+            using var conn = db.connect();
+            string sql = @"SELECT u.id, u.nama, r.nama AS role, d.nama AS divisi
+                           FROM user u
+                           JOIN role r ON r.id = u.id_role
+                           LEFT JOIN divisi d ON d.id = u.id_divisi
+                           WHERE u.id = @id AND u.id_role = 2;";
+
+            return await conn.QueryFirstOrDefaultAsync<UserDTO>(sql, new { id });
+        }
+
+        public async Task<bool> Update(int id, UserOTD data)
+        {
+            using var conn = db.connect();
+            int? divisiId = data.id_divisi > 0 ? data.id_divisi : null;
+
+            string sql = @"UPDATE user 
+                           SET nama = @nama, 
+                               id_divisi = @id_divisi 
+                           WHERE id = @id AND id_role = 2;";
+
+            var result = await conn.ExecuteAsync(sql, new
             {
-                string sql = @"SELECT u.id, u.nama, r.nama AS role, d.nama AS divisi
-                       FROM user u
-                       JOIN role r ON r.id = u.id_role
-                       LEFT JOIN divisi d ON d.id = u.id_divisi
-                       WHERE u.id_role = 2;";
-                var result = await conn.QueryAsync<UserDTO>(sql);
-                return result.ToList();
-            }
+                id,
+                nama = data.nama,
+                id_divisi = divisiId
+            });
+
+            return result > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using var conn = db.connect();
+            string sql = "DELETE FROM user WHERE id = @id AND id_role = 2;";
+            var result = await conn.ExecuteAsync(sql, new { id });
+
+            return result > 0;
         }
     }
 }
